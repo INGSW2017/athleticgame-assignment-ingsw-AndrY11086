10b1f97d3d3903128bb0517cbd76f0ee3a3a7d17
valentina <andreapuleio86@gmail.com>
2017-10-18 13:16:20 +0200

    Miss getWinnerTest2()

--- AthleticGameAssignmentINGSW/test/it/unical/test/AthleticGameBasicTest.java
+++ AthleticGameAssignmentINGSW/test/it/unical/test/AthleticGameBasicTest.java
@@ -1,5 +1,26 @@
 package it.unical.test;
 
+import java.time.Instant;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import it.unical.asde.AthleticGame;
+
 public class AthleticGameBasicTest {
 
+	@Test
+	public void addArrival(){
+		AthleticGame ag = new AthleticGame("sport");
+		ag.addArrival("partecipante", Instant.now());
+		Assert.assertEquals(1,ag.getArrivals().size());
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void getPartecipantTime(){
+		AthleticGame ag = new AthleticGame("sport");
+		
+		ag.getParecipiantTime("");
+	}
+	
 }
